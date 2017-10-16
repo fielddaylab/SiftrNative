76ea905365c0ddd4e89a9bd3bf5fcb61f08dc670
Michael Tolly <miketolly@gmail.com>
2017-10-16 12:18:33 -0500

    Add/link camera and camera roll libs

--- android/app/src/main/java/com/siftrnative/MainApplication.java
+++ android/app/src/main/java/com/siftrnative/MainApplication.java
@@ -3,6 +3,7 @@ package com.siftrnative;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.github.yamill.orientation.OrientationPackage;
 import com.rnfs.RNFSPackage;
 import com.horcrux.svg.SvgPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RCTCameraPackage(),
           new OrientationPackage(),
           new RNFSPackage(),
           new SvgPackage(),
