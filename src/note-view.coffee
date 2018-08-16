fielddaylab/SiftrNative

commit a180effc736d0a417b78899600792de43efe5791

Show photos in pending upload notes

src/note-view.coffee
@@ -20,6 +20,7 @@ T = require 'prop-types'
 , BackHandler
 , Modal
 , TouchableWithoutFeedback
+, Platform
 } = require 'react-native'
 import FitImage from 'react-native-fit-image'
 import Hyperlink from 'react-native-hyperlink'
@@ -582,9 +583,7 @@ class SiftrNoteView extends React.Component
       @loadExtra nextProps.note
 
   loadExtra: (note = @props.note) ->
-    if note.pending
-      console.warn JSON.stringify note
-      return
+    return if note.pending
     # load comments
     @props.auth.getNoteCommentsForNote
       note_id: note.note_id
@@ -817,17 +816,24 @@ class SiftrNoteView extends React.Component
   render: ->
     # TODO move to CacheMedia
 
-    photoIDs = [@props.note.media_id]
-    if (@state.field_data? and @props.fields?)
-      for field in @props.fields
-        if field.field_type is 'MEDIA'
-          data =
-            d for d in @state.field_data when d.field_id is field.field_id
-          if data.length > 0
-            photoIDs.push data[0].media.media_id
-
-    photoURLs =
-      (@state["media#{media_id}"] or '') for media_id in photoIDs
+    if @props.note.pending
+      photoIDs = []
+      photoURLs =
+        for f in @props.note.files
+          if Platform.OS is 'ios'
+            "#{@props.note.dir}/#{f.filename}"
+          else
+            "file://#{@props.note.dir}/#{f.filename}"
+    else
+      photoIDs = [@props.note.media_id]
+      if (@state.field_data? and @props.fields?)
+        for field in @props.fields
+          if field.field_type is 'MEDIA'
+            data =
+              d for d in @state.field_data when d.field_id is field.field_id
+            if data.length > 0
+              photoIDs.push data[0].media.media_id
+      photoURLs = (@state["media#{media_id}"] or '') for media_id in photoIDs
 
     <ScrollView
       ref={(sv) => @scrollView = sv}

src/map.js
@@ -469,7 +469,9 @@ export class SiftrMap extends React.Component {
     ).concat(this.props.pendingNotes.map(({dir, json}) => {
       json = JSON.parse(json);
       let note = new Note(json);
+      if (note.game_id !== this.props.game_id) return null;
       note.pending = true;
+      note.dir = dir.path;
       note.files = json.files;
       return <MapNote
         key={dir.name}
@@ -482,7 +484,7 @@ export class SiftrMap extends React.Component {
         onMouseLeave={this.props.onMouseLeave}
         thumbHover={this.props.thumbHover}
       />;
-    }));
+    }).filter((x) => x != null));
   }
 
   // @ifdef NATIVE

src/siftr-view.coffee
@@ -752,6 +752,7 @@ export SiftrView = createClass
       }
       thumbHover={@state.thumbHover?.note_id}
       tags={@state.tags}
+      game_id={@props.game.game_id}
     />
 
   renderThumbnails: ->

commit 9f5ec0e2c2b2d4bdf9b8f67d55ba6ab757480e1e

Fix note photos not showing (broke in a180eff)

src/note-view.coffee
@@ -833,7 +833,8 @@ class SiftrNoteView extends React.Component
               d for d in @state.field_data when d.field_id is field.field_id
             if data.length > 0
               photoIDs.push data[0].media.media_id
-      photoURLs = (@state["media#{media_id}"] or '') for media_id in photoIDs
+      photoURLs =
+        (@state["media#{media_id}"] or '') for media_id in photoIDs
 
     <ScrollView
       ref={(sv) => @scrollView = sv}