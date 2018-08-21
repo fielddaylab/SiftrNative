6428871fe284c938e26f683add56b25c738a0413
Michael Tolly <miketolly@gmail.com>
2018-08-21 12:35:31 -0500

    Fix map marker position + number position

--- src/map.js
+++ src/map.js
@@ -126,6 +126,7 @@ class MapCluster extends React.Component {
         latitude: this.props.lat,
         longitude: this.props.lng,
       }}
+      anchor={{x: 0.5, y: 0.5}}
       title=""
       description=""
       pinColor="black"
@@ -176,7 +177,7 @@ class MapCluster extends React.Component {
             stroke="black"
             fill="white"
             x={r + 5}
-            y={Platform.OS === 'ios' ? 1 : 4}
+            y={(w + 3) * 0.65}
             fontSize={w * (2/3)}
             fontWeight={Platform.OS === 'ios' ? '900' : 'bold'}
           >{this.props.cluster.note_count}</SvgText>
