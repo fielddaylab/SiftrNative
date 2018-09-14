716534949e19db740f5beab487e9f38be4c1234a
Michael Tolly <miketolly@gmail.com>
2018-09-14 16:23:53 -0500

    Include URL when opening siftr via ios app banner

--- web/index.php
+++ web/index.php
@@ -4,7 +4,9 @@
   <meta charset="utf-8">
   <title>Siftr</title>
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1" />
-  <meta name="apple-itunes-app" content="app-id=1169649470">
+  <meta name="apple-itunes-app" content="app-id=1169649470, app-argument=<?=
+    (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"
+  ?>">
   <meta name="google-play-app" content="app-id=org.siftr.client">
   <script type="text/javascript" src="dist.js"></script>
   <link rel="stylesheet" type="text/css" href="styles.css">
